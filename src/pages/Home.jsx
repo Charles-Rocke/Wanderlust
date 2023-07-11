9be4817813307b864aa41249fcfd21337b99ef2f
SpOOk <spook.devv@gmail.com>
2023-07-11 12:59:11 -0400

    Nav bar with NavLink

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Home() {
-  return <div>Wanderlust</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Wanderlust</h1>
+    </div>
+  );
 }
 
 export default Home;
