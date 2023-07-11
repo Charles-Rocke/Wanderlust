b6203b0742c95a4ce432de4bc4c7a839aceaf098
SpOOk <spook.devv@gmail.com>
2023-07-11 17:25:46 -0400

    Added routes and child routes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,15 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      {/* the alternative to the children prop is Outlet for routing */}
+      <Outlet />
       <Footer />
     </div>
   );
